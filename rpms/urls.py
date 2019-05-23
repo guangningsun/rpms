59eccd97e98ba41cc0cc364c2cab68fa51d47b49
guangningsun <sun-guangning@126.com>
2019-05-24 00:19:16 +0800

    定义所有方法，并修改READE增加api描述

--- rpms/urls.py
+++ rpms/urls.py
@@ -26,15 +26,37 @@ urlpatterns = [
     url(r'^get_user_info_by_id/', views.get_user_info_by_id),
     url(r'^modify_user/', views.modify_user),
     url(r'^remove_user/', views.remove_user),
+   
     url(r'^manage_user', views.manage_student),
     url(r'^manage_payment', views.manage_payment),
     url(r'^manage_student', views.manage_student),
     url(r'^manage_payment_class', views.manage_payment_class),
     url(r'^manage_class', views.manage_class),
+   
     url(r'^create_student/', views.create_student),
     url(r'^get_all_student_info/', views.get_all_student_info),
     url(r'^get_student_info_by_id/', views.get_student_info_by_id),
     url(r'^modify_student/', views.modify_student),
     url(r'^remove_student/', views.remove_student),
+   
+    url(r'^create_payment', views.create_payment),
+    url(r'^remove_payment', views.remove_payment),
+    url(r'^get_all_payment_info', views.get_all_payment_info),
+    url(r'^modify_payment', views.modify_payment),
+    url(r'^get_payment_list_by_stu_id_card',
+        views.get_payment_list_by_stu_id_card),
+
+    url(r'^remove_payment_class', views.remove_payment_class),
+    url(r'^create_payment_class', views.create_payment_class),
+    url(r'^get_all_payment_class_info', views.get_all_payment_class_info),
+    url(r'^modify_payment_class', views.modify_payment_class),
+
+    url(r'^create_class', views.create_class),
+    url(r'^remove_class', views.remove_class),
+    url(r'^get_all_class_info', views.get_all_class_info),
+    url(r'^modify_class', views.modify_class),
+
     url(r'^student_login_api', views.student_login_api),
+    url(r'^get_student_info_summary_api',
+        views.get_student_info_summary_api),
 ]
